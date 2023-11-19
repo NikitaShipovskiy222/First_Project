f52d75d01e2e3f2cffa9037e2f8a498fe286629e
nikita shipovskiy <nikita04.shipovskiy@gmail.com>
2023-11-19 16:38:16 +0100

    Initial Commit

---
+++ First_Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  First_Project
+//
+//  Created by Nikita Shipovskiy on 19/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
